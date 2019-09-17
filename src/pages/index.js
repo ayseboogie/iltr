7dbc37562b24f4961c3fcff0e852e5b65b707f7a
Ayse <ayse.stinnett@gmail.com>
2019-09-17 14:57:48 -0500

    carousel rend

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ import Header from "../components/header";
 import "./index.css";
 import GatsbyConfig from "../../gatsby-config"
 import {Helmet} from "react-helmet";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from "./Carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 const IndexPage = () => (
   <Layout>
@@ -17,6 +21,7 @@ const IndexPage = () => (
       {/* <SEO title={GatsbyConfig.siteMetadata.title} /> */}
      <Helmet><title>Newsroom, Trade, & Advertising</title></Helmet>
      <h1 className="page-title" style={{textAlign: "center"}}>Newsroom, Trade, & Advertising</h1>
+     <Carousel />
       <div className="column-wrapper">
         <div className="left-column">
           <MainContent />
