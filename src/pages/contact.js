9ccf490f88b283e6d61756dbc1f602ab0fd994e8
ayseboogie <ayse.stinnett@gmail.com>
2019-09-24 02:45:49 -0500

    pre-contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Header from "../components/header";
 import RelativeLinks from "../components/relativeLinks";
-import Card from 'react-bootstrap/Card';
-import CardDeck from 'react-bootstrap/CardDeck';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Helmet from "react-helmet";
+import Footer from "../components/footer"
+
 
 class contact extends Component {
   render() {
@@ -12,69 +14,60 @@ class contact extends Component {
       <div>
         <Helmet><title>Contact Us</title></Helmet>
           <Header />
-          <div style={{backgroundColor: "whitesmoke"}}>
-        <h2 style={{textAlign: "center", paddingTop: "5%", fontSize: "80px"}}>Contact Us</h2>
-       
-        <br />
-        <div style={{alignContent: "center", textAlign: "center"}}>
-<CardDeck>
-    <table>
-        <tr>
-            <td>&nbsp;</td>
-            <td style={{verticalAlign: "top"}}>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="Guidebooks" src="https://www.unchartedbackpacker.com/wp-content/uploads/2016/01/The-Variety-2-730x659.jpg" />
-  <Card.Body>
-    <Card.Title><b>Guidebook Feedback</b></Card.Title>
-    <Card.Text style={{fontSize: "14px"}}>
-      Got something to say about our guidebooks? We'd love to hear your suggestions, corrections, compliments and complaints.
-    </Card.Text>
-    <Button variant="primary">Guidebook Feedback</Button>
-  </Card.Body>
-</Card>
-            </td>
-            <td style={{verticalAlign: "top"}}>
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="Guidebooks" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOMsXJU-X3xWaJr_FugbaUJH73vdcg9wtebH462_u44e8xyMlw" />
-  <Card.Body>
-    <Card.Title><b>Help & Support</b></Card.Title>
-    <Card.Text style={{fontSize: "14px"}}>
-       Signing in / Registering on our website<br/>
-       Online shop orders<br/>
-       Travel BookingseBooks & Mobile apps<br/>
-       A mistake on our website<br/>
-       Magazine & Newsletter subscriptions<br/>
-       Thorn Tree Forum
-       <br/><br/>
-    </Card.Text>
-    <Button variant="primary">Help & Support</Button>
-  </Card.Body>
-</Card>
-            </td>
-            <td style={{verticalAlign: "top"}}>
-                <Card style={{ width: '18rem' }}>
-  <Card.Img variant="Guidebooks" src="https://lonelyplanetstatic.imgix.net/copilot/images/b2ZmaWNlLTEuanBnV2VkIE1hciAyOSAyMDE3IDE0OjU5OjM1IEdNVCswMDAwIChVVEMp.jpg" />
-  <Card.Body>
-    <Card.Title><b>Your Business</b></Card.Title>
-    <Card.Text style={{fontSize: "14px"}}>
-      Do you have a business you'd like listed in Lonely Planet? Let us know and we'll pass it on to our authors to check out.
-<br/>
-Are you already listed but need to update your details? Do it right here.
-    </Card.Text>
-    <br/>
-    <Button variant="primary">Business Listings</Button>
-  </Card.Body>
-</Card>
-            </td>
-        </tr>
-    </table>
-</CardDeck>
+          <div style={{backgroundColor: "black", color: "white", paddingLeft: "4%", paddingRight: "4%"}}>
+          <Form>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
 
-           
-                
-                
-        </div>
-       </div>
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+  </Form.Group>
+
+  <Form.Group controlId="formGridAddress2">
+    <Form.Label>Address 2</Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Control as="select">
+        <option>Choose...</option>
+        <option>...</option>
+      </Form.Control>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group id="formGridCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</div>
+<Footer />
       </div>
     );
   }
---
+++ src/pages/videos.js
@@ -0,0 +1,116 @@
+import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer"
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Image from 'react-bootstrap/Image'
+import Col from 'react-bootstrap/Col'
+
+function vidoes() {
+return (
+    <div style={{backgroundColor: "black", width: "100%", height: "100%"}}>
+   <Header />
+   <div style={{textAlign: "center"}}>
+<iframe width="90%" height="600" src="https://www.youtube.com/embed/-GgUOTRxF4w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+   <CardDeck style={{paddingLeft: "4%", paddingRight: "4%"}}>
+  <Card style={{backgroundColor: "black"}}>
+    <Card.Body style={{backgroundColor: "black"}}>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/ZQ015OmRRS0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+  <Card style={{backgroundColor: "black"}}>
+       <Card.Body style={{backgroundColor: "black"}}>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/UcN8ENDMgMA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+</CardDeck>
+{/* 2 */}
+<CardDeck style={{paddingLeft: "4%", paddingRight: "4%"}}>
+  <Card style={{backgroundColor: "black"}}>
+    <Card.Body style={{backgroundColor: "black"}}>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/FQVYKF7f6cY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+  <Card style={{backgroundColor: "black"}}>
+       <Card.Body style={{backgroundColor: "black"}}>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/dGhTWrZQTEg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+</CardDeck>
+{/* 3 */}
+<CardDeck style={{paddingLeft: "4%", paddingRight: "4%"}}>
+  <Card style={{backgroundColor: "black"}}>
+    <Card.Body style={{backgroundColor: "black"}}>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/bTAXx1wHVi4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+  <Card style={{backgroundColor: "black"}}>
+       <Card.Body style={{backgroundColor: "black"}}>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/rLOkrACvExo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+</CardDeck>
+{/* 4 */}
+<CardDeck style={{paddingLeft: "6%", paddingRight: "4%", textAlign: "center", alignContent: "center"}}>
+  <Card style={{backgroundColor: "black"}}>
+    <Card.Body style={{backgroundColor: "black"}}>
+    <iframe width="300" height="315" src="https://www.youtube.com/embed/aPS-zbw5S9k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+  <Card style={{backgroundColor: "black"}}>
+       <Card.Body style={{backgroundColor: "black"}}>
+       <iframe width="300" height="315" src="https://www.youtube.com/embed/7DQMR_ftqOA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+  <Card style={{backgroundColor: "black"}}>
+       <Card.Body style={{backgroundColor: "black"}}>
+       <iframe width="300" height="315" src="https://www.youtube.com/embed/RvR3q1hbt9g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+</CardDeck>
+{/* 5 */}
+<CardDeck style={{paddingLeft: "4%", paddingRight: "4%"}}>
+  <Card style={{backgroundColor: "black"}}>
+    <Card.Body style={{backgroundColor: "black"}}>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/g7lcDt4HAms" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+  <Card style={{backgroundColor: "black"}}>
+       <Card.Body style={{backgroundColor: "black"}}>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/6AIzvc9-1As" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+</CardDeck>
+{/* 6 */}
+<CardDeck style={{paddingLeft: "4%", paddingRight: "4%"}}>
+  <Card style={{backgroundColor: "black"}}>
+    <Card.Body style={{backgroundColor: "black"}}>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/9HvpNPOaDTc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+  <Card style={{backgroundColor: "black"}}>
+       <Card.Body style={{backgroundColor: "black"}}>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/amJwNwxT6-8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+</CardDeck>
+{/* 7 */}
+<CardDeck style={{paddingLeft: "4%", paddingRight: "4%"}}>
+  <Card style={{backgroundColor: "black"}}>
+    <Card.Body style={{backgroundColor: "black"}}>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/XPwtc-WtsOc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+  <Card style={{backgroundColor: "black"}}>
+       <Card.Body style={{backgroundColor: "black"}}>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/9n5_yACV9Os" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </Card.Body>
+  </Card>
+</CardDeck>
+   <Footer />
+    </div>
+)
+}
+
+export default vidoes
