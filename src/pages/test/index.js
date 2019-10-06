50dab947c2646605caaa110915671111ef2e885e
Ayse Stinnett <ayse.stinnett@lonelyplanet.com>
2019-10-06 13:57:05 -0500

    grouping files in folders

---
+++ src/pages/test/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Header from "../../components/header";
+import RelativeLinks from "../../components/relativeLinks";
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Helmet from "react-helmet";
+import Footer from "../../components/footer"
+import Layout from "../../components/layout";
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+
+function test() {
+  return (
+    <Layout>
+      <div style={{backgroundColor: "black", height: "100vh"}}>
+          <Header />
+          <Form style={{backgroundColor: "black", color: "white", paddingLeft: "4%", paddingRight: "4%"}}v>
+          <Form.Row>
+    <Form.Group as={Col} controlId="formGridFirstName">
+      <Form.Label>First Name</Form.Label>
+      <Form.Control placeholder="Enter First Name" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridLastName">
+      <Form.Label>Last Name</Form.Label>
+      <Form.Control placeholder="Enter Last Name" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Email Address" />
+    </Form.Group>
+
+  <Form.Group controlId="formGridSubject">
+    <Form.Label>Subject</Form.Label>
+    <Form.Control placeholder="Enter Subject" />
+  </Form.Group>
+  <Form.Group controlId="formGridMessage">
+    <Form.Label>Message</Form.Label>
+    <Form.Control placeholder="Enter Message" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+      </div>
+      <Footer />
+      </Layout>
+  )
+  }
+ 
+export default test;
